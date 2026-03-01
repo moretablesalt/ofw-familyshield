af091018b669c15d762806a7b0ddb27cf173ebce
Mark Tiu <mark.tiu@prudentialguarantee.com>
2026-03-01 15:31:29 +0800

    Make stepper work.

--- src/app/feature/family-shield/component/form/form.ts
+++ src/app/feature/family-shield/component/form/form.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StepperService } from '../../../../layout/stepper/stepper.service';
 
 @Component({
   selector: 'app-form',
@@ -7,4 +8,10 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './form.html',
   styleUrl: './form.css',
 })
-export class Form {}
+export class Form {
+  private stepperService = inject(StepperService);
+
+  constructor() {
+    this.stepperService.setActive(1);
+  }
+}
