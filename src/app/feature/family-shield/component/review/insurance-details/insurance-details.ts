6171aed48d812b9120daa4e74d18bc3b14e14a63
Mark Tiu <mark.tiu@prudentialguarantee.com>
2026-03-03 08:19:35 +0800

    Use computed family and personal units to dynamically calculate coverage and benefits in InsuranceDetails.

--- src/app/feature/family-shield/component/review/insurance-details/insurance-details.ts
+++ src/app/feature/family-shield/component/review/insurance-details/insurance-details.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { QuoteStateService } from '../../../state/quote-state.service';
+import { CivilStatus } from '../../../../../core/enum/civil-status.enum';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-insurance-details',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './insurance-details.html',
   styleUrl: './insurance-details.css',
 })
-export class InsuranceDetails {}
+export class InsuranceDetails {
+  private quoteState = inject(QuoteStateService);
+
+  familyUnits = computed(() => this.quoteState.request().familyLevel);
+  personalUnits = computed(() => this.quoteState.request().personalLevel);
+
+  civilStatus = computed(() => this.quoteState.request().civilStatus);
+
+  showPackage1 = computed(() => this.civilStatus() === CivilStatus.Married);
+
+  showPackage2 = computed(
+    () => this.civilStatus() !== CivilStatus.Married || this.civilStatus() === CivilStatus.Single,
+  );
+}
