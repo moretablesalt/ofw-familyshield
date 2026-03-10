5ef05fe762d0f8ebe1b732d34b76387c1cc58549
Mark Tiu <mark.tiu@prudentialguarantee.com>
2026-03-10 18:49:17 +0800

    Update environment variables to use production API endpoints for HOST and ADDRESS services

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
-  HOST_URL: 'https://appservice-uat-ofw-backend-hefng6asdjcncdc6.southeastasia-01.azurewebsites.net',
-  ADDRESS_API_URL: 'https://ap-uat-address-esfkf3g3bdcsdhcd.southeastasia-01.azurewebsites.net',
+  HOST_URL:
+    'https://appservice-prod-ofw-backend-f0dgabcxhye0g7dz.southeastasia-01.azurewebsites.net',
+  ADDRESS_API_URL:
+    'https://appservice-prod-address-ekfzhqdsbebjhbhk.southeastasia-01.azurewebsites.net',
 };
