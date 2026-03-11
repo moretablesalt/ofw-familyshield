8a7f1e2496806d469b955e53bdfbb131ff29a9e8
Mark Tiu <mark.tiu@prudentialguarantee.com>
2026-03-11 18:22:01 +0800

    BDOIOFWFAM-5: Removed unused imports and explicitly define the type of breakdown.

--- src/app/feature/family-shield/model/quote-request.model.ts
+++ src/app/feature/family-shield/model/quote-request.model.ts
@@ -1,5 +1,3 @@
-import { CivilStatus } from '../../../core/enum/civil-status.enum';
-
 export interface QuoteRequest {
   policyHolderCivilStatus: 'SINGLE' | 'MARRIED' | 'WIDOWED' | 'SEPARATED' | null;
   familyUnit: number;
